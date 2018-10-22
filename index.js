63b93c24ac94b2e0dba55a06b285d4eef2d4e657
kendyl93 <p.stanecki93@gmail.com>
2018-10-22 08:54:48 +0200

    Refactoring

--- index.js
+++ index.js
@@ -1,149 +1,108 @@
-const compareLangKeys = () => {
-  // lib is needed to show deuplicated keys
-  const jsonValidator = require('json-dup-key-validator');
+const jsonValidator = require('json-dup-key-validator');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
-  //requiring path and fs modules
-  const path = require('path');
-  const fs = require('fs');
-  const directoryPath = path.join(__dirname, 'locales');
+const { promisify } = util;
+const readDirAsync = promisify(fs.readdir);
+const readFileAsync = promisify(fs.readFile);
 
-  const mapAllFiles = (files, filesContent) => {
-    const isJsonFile = (JSONdata, file) => {
-      const singleFilesContent = [];
+const laodFilesNames = async directoryPath => {
+  const filesNames = await readDirAsync(directoryPath);
+  return filesNames;
+};
 
-      for (const key in JSONdata) {
-        singleFilesContent.push(key);
-      }
-      filesContent.push({
-        fileName: file,
-        fileKeys: singleFilesContent
-      });
-    };
+const loadFilesContent = async (filesNames, dir) => {
+  const filesData = [];
+  const errorsList = [];
 
-    const isNotJsonFile = () => {
-      notJSON++;
-      filesContent.push({
-        fileName: 'null',
-        fileKeys: []
-      });
-    };
+  await Promise.all(
+    filesNames.map(async fileName => {
+      if (fileName.endsWith('.json')) {
+        fileData = await readFileAsync(`./${dir}/${fileName}`, 'utf8');
+        parsedFileData = await jsonValidator.parse(fileData, true);
+        filesData.push({
+          fileName,
+          fileData: parsedFileData
+        });
 
-    const loadFiles = (file, isJsonFile, isNotJsonFile) => {
-      // load file if it is a JSON
-      if (file.includes('.json')) {
-        const JSONdata = require(`./locales/${file}`);
-
-        const isDuplicated = new Promise((resolve, reject) => {
-          fs.readFile(`./locales/${file}`, 'utf8', (err, data) => {
-            if (err) {
-              reject(err);
-            } else {
-              const validateDuplicates = jsonValidator.validate(data, false);
-              if (validateDuplicates) {
-                resolve(
-                  `[DUPLICATED] ${jsonValidator.validate(
-                    data,
-                    false
-                  )} => in ${file}`
-                );
-              }
-            }
+        const duplicatedKey = await jsonValidator.validate(fileData, false);
+        if (duplicatedKey) {
+          errorsList.push({
+            type: '[DUPLICATED]',
+            key: duplicatedKey,
+            fileName: fileName
           });
-        });
-        isDuplicated.then(res => {
-          console.info(res);
-        });
-        isDuplicated.catch(rej => {
-          console.info(rej);
-        });
-
-        isJsonFile(JSONdata, file);
-      } else {
-        isNotJsonFile;
+        }
       }
-    };
+    })
+  );
 
-    files.forEach(file => {
-      loadFiles(file, isJsonFile, isNotJsonFile);
-    });
-  };
-  const countKeysBetweenFiles = filesContent => {
-    const singleFinalKeysArr = [];
-    for (const key in filesContent) {
-      const filesContentKeys = filesContent[key]['fileKeys'];
-      for (const k in filesContentKeys) {
-        singleFinalKeysArr.push(filesContentKeys[k]);
-      }
+  return [filesData, errorsList];
+};
+
+const getAllFilesData = async dir => {
+  const directoryPath = path.join(__dirname, dir);
+  const filesNames = await laodFilesNames(directoryPath);
+  return await loadFilesContent(filesNames, dir);
+};
+
+const createKeysList = filesData => {
+  const keysList = [];
+  for (const key in filesData) {
+    const filesKeysList = filesData[key].fileData;
+    for (const key in filesKeysList) {
+      keysList.push(key);
     }
-    const map = singleFinalKeysArr.reduce((prev, cur) => {
-      prev[cur] = (prev[cur] || 0) + 1;
-      return prev;
-    }, {});
+  }
 
-    return map;
-  };
+  return keysList;
+};
+const countKeysInFiles = keysList => {
+  const countedKeys = keysList.reduce((prev, cur) => {
+    prev[cur] = (prev[cur] || 0) + 1;
+    return prev;
+  }, {});
+  return countedKeys;
+};
 
-  const countNotJsonFiles = files => {
-    let nonJson = 0;
-    files.forEach(f => {
-      if (!f.includes('.json')) {
-        nonJson++;
-      }
-    });
-    return nonJson;
-  };
+const countJsonFiles = filesData => filesData.length;
 
-  const dspFinalResults = (finalCountedKeys, jsonFiles, filesContent) => {
-    for (const key in finalCountedKeys) {
-      // console.log(notJSON)
-      if (finalCountedKeys[key] < jsonFiles) {
-        const bad_val = `[BAD] ${key}`;
-
-        for (const k in filesContent) {
-          // in case Key not seen
-
-          const filesContentKeys = filesContent[k]['fileKeys'];
-          const fileNameFromK = filesContent[k];
-          const { fileName } = fileNameFromK;
-          if (
-            filesContentKeys.length !== 0 &&
-            !filesContentKeys.includes(key)
-          ) {
-            console.info(`${bad_val} => not seen in ${fileName}`);
-          }
-          for (const n in filesContentKeys) {
-            // throw file name with [BAD]
-            if (
-              key === filesContentKeys[n] &&
-              !filesContentKeys.includes(key)
-            ) {
-              console.info(`${bad_val} => in ${fileName}`);
-            }
-          }
+const findBadKeys = (countedKeysList, countedFiles) => {
+  Object.keys(countedKeysList).forEach(key => {
+    if (countedKeysList[key] < countedFiles) {
+      for (const number in filesData) {
+        const { fileData: singleFileData } = filesData[number];
+        const { fileName } = filesData[number];
+        if (key in singleFileData) {
+          errorsList.push({
+            type: '[BAD]',
+            key: key,
+            fileName: fileName
+          });
         }
       }
     }
-  };
-
-  fs.readdir(directoryPath, (err, files) => {
-    //handling error
-    if (err) {
-      return console.info('Unable to scan directory: ' + err);
-    }
-
-    const filesContent = []; // final array with the keys from JSON files
-    const noJsonFiles = countNotJsonFiles(files);
-    const jsonFiles = files.length - noJsonFiles; // only JSON files
-
-    // 1. Map all the files and make one object with the name and content of the file
-    mapAllFiles(files, filesContent);
-
-    // 2. count number of the same keys between files
-    const finalCountedKeys = countKeysBetweenFiles(filesContent);
-
-    // 3. check and display results
-    dspFinalResults(finalCountedKeys, jsonFiles, filesContent);
   });
 };
 
-compareLangKeys();
+const printErrorMessages = errorsList => {
+  if (errorsList !== undefined && errorsList.length !== 0) {
+    errorsList.map(error => {
+      console.log(`${error.type} key: ${error.key} in => ${error.fileName}`);
+    });
+  } else {
+    console.log('[OK]');
+  }
+};
+
+compareLanguageKeys = async dir => {
+  const [filesData, errorsList] = await getAllFilesData(dir);
+  const keyList = createKeysList(filesData);
+  const countedKeysList = countKeysInFiles(keyList);
+  const countedFiles = countJsonFiles(filesData);
+  findBadKeys(countedKeysList, countedFiles);
+  printErrorMessages(errorsList);
+};
+
+compareLanguageKeys('locales');
